4a1a7014908778ebc78b6ac7061fbec6d7625e07
smarya <imadurgaprasad@gmail.com>
2018-03-16 00:11:20 +0530

    added functions.go

--- maps.go
+++ maps.go
@@ -1,6 +1,16 @@
 package main
 
+//code at  https://play.golang.org/p/TtljtOXV3HL
+import (
+	"fmt"
+)
+
 func main() {
 	presAge := make(map[string]int)
 	presAge["Roosevelt"] = 42
+	fmt.Println(presAge["Roosevelt"])
+	presAge["Jhon F. Kennedy"] = 43
+	fmt.Println(len(presAge))
+	delete(presAge, "Jhon F. Kennedy")
+	fmt.Println(len(presAge))
 }
