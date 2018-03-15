058fee11b41c46dc6df3a192fdb77a717e721a4c
smarya <imadurgaprasad@gmail.com>
2018-03-15 23:24:26 +0530

    uncommented variables.go

--- variables.go
+++ variables.go
@@ -1,29 +1,29 @@
 package main
 
-// code link https://play.golang.org/p/W8vGaJ3Z5jd
-// func main() {
-// 	const pi = 3.145
-// 	var x int = 20
-// 	var y float64 = 2.2
-// 	z := "Hello World"
-// 	fmt.Println(x, " ", y, " ", z, " ", pi)
+code link https://play.golang.org/p/W8vGaJ3Z5jd
+func main() {
+	const pi = 3.145
+	var x int = 20
+	var y float64 = 2.2
+	z := "Hello World"
+	fmt.Println(x, " ", y, " ", z, " ", pi)
 
-// 	//Declaring multiple variables in a single line
-// 	var (
-// 		varA = 10
-// 		VarB = 20
-// 	)
-// 	fmt.Println(varA, "  ", VarB)
+	//Declaring multiple variables in a single line
+	var (
+		varA = 10
+		VarB = 20
+	)
+	fmt.Println(varA, "  ", VarB)
 
-// 	var myName = "Durga PRasad"
-// 	fmt.Println(len(myName), "is the length of", myName)
-// 	//boolean
-// 	var over bool = true
-// 	print(over, "\n")
-// 	//To get a data type of a variable we use %T
-// 	fmt.Printf("%T ", varA)
-// 	// %f for float  .3f for precission
-// 	// %t for bool
-// 	// %d for int
-// 	fmt.Print("data type of varA \n")
-// }
+	var myName = "Durga PRasad"
+	fmt.Println(len(myName), "is the length of", myName)
+	//boolean
+	var over bool = true
+	print(over, "\n")
+	//To get a data type of a variable we use %T
+	fmt.Printf("%T ", varA)
+	// %f for float  .3f for precission
+	// %t for bool
+	// %d for int
+	fmt.Print("data type of varA \n")
+}
